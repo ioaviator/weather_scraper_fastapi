e16673a0700369ff721999d07ea3bc92d91051a6
ioaviator <kingsolomonifeanyi@gmail.com>
2025-07-26 15:03:06 +0100

    fix html connection to fastapi backend

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from typing import Dict, List, Optional
+
 import uvicorn
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from config import api_url, g_sheet_url
 
 app = FastAPI()
 
@@ -12,6 +16,28 @@ app.mount("/templates/static", StaticFiles(directory="templates/static"), name="
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get('/get_weather')
+def get_weather_data(state_1:str,state_2:Optional[str] = None,
+                     state_3:Optional[str] = None):
+  
+  weather_url:str = api_url
+
+  states:List[str] = [state_1, state_2, state_3]
+  
+  # check for empty state names
+  state_values:List = [value for value in states if value.strip()]
+
+  if len(state_values) == 0:
+    return {
+      "message": "No state was given",
+      "status": 200
+    }
+  
+  return {
+     "state": state_values,
+     "status": 200
+  }
 
 
 # if __name__ == "__main__":
